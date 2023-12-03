94ba0f305283c4f869702d1060b05572348df413
jasur <251tojiboyev@gmail.com>
2023-12-03 20:54:31 +0500

    GUESS THE NUMBER

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+def guess_the_number():
+    number = random.randint(1, 100)
+    print("Men 1 dan 100 gacha son o'yladim. Topa olasizmi?")
+
+    while True:
+        guess = int(input("Sizning taxminingiz: "))
+
+        if guess < number:
+            print("Xato. Men o'ylagan son bundan katta.")
+        elif guess > number:
+            print("Xato. Men o'ylagan son bundan kichik.")
+        else:
+            print(f"Tabriklayman! Men {number} sonini o'ylagan edim.")
+            break
+
+guess_the_number()
